82dcd8902b44b7a32c30beb6a3398cb336224cf7
aakashsr <aakashsri.dev@gmail.com>
2019-09-12 21:10:57 +0530

    added the checkbox and some helper text

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-import React from 'react';
-import './App.css';
+import React from "react";
 
 function App() {
   return (
-   "hello World!"
+    <div>
+      <input type="checkbox" />
+      <p>Placeholder text here</p>
+
+      <input type="checkbox" />
+      <p>Placeholder text here</p>
+
+      <input type="checkbox" />
+      <p>Placeholder text here</p>
+
+      <input type="checkbox" />
+      <p>Placeholder text here</p>
+    </div>
   );
 }
 
