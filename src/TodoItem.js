4202cd264170e548667afdc88e9a9f130ca5139f
aakashsr <aakashsri.dev@gmail.com>
2019-09-13 08:32:44 +0530

    added onChange handler to input element(it's doing nothing special yet

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -3,7 +3,11 @@ import React from "react";
 function TodoItem({ item }) {
   return (
     <div className="todo-item">
-      <input type="checkbox" checked={item.completed} />
+      <input
+        type="checkbox"
+        checked={item.completed}
+        onChange={() => console.log("clicked")}
+      />
       <p>{item.text}</p>
     </div>
   );
