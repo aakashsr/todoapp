9f3bd059d919365e24ba7a538b4501460bb40a2d
aakashsr <aakashsri.dev@gmail.com>
2019-09-16 19:38:51 +0530

    added filter checkbox and text inside Header component

--- src/Header.js
+++ src/Header.js
@@ -1,11 +1,15 @@
-import React from 'react';
+import React from "react";
 
 const Header = () => {
-    return (
-        <div className='header'>
-            <h3>My Todo</h3>
-        </div>
-    );
-}
+  return (
+    <div className="header">
+      <h3>My Todo</h3>
+      <label>
+        <input type="checkbox" name="checkbox" value="" />
+        Hide those which has been completed.
+      </label>
+    </div>
+  );
+};
 
 export default Header;
